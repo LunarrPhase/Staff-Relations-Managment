85b7fa9087921792536d52ee45bfe94264f82de9
chloe900 <151669713+chloe900@users.noreply.github.com>
2024-04-29 12:36:55 +0200

    added manage-users.js

--- src/admin-main-page.js
+++ src/admin-main-page.js
@@ -33,3 +33,8 @@ mealInfo.addEventListener('click', function(event) {
     window.location.href = 'create-meal.html'; 
     console.log('Meal info clicked!');
 });
+
+const manageUsers = document.getElementById('manage-users')
+manageUsers.addEventListener('click', () => {
+  window.location.href = 'manage-users.html'
+})
