9d28df8ca3d86cbea3ff495bdfa043db8661dd83
LunarrPhase <137800566+LunarrPhase@users.noreply.github.com>
2024-04-29 12:29:57 +0200

    Update timesheet.js

--- src/timesheet.js
+++ src/timesheet.js
@@ -22,6 +22,39 @@ document.addEventListener("DOMContentLoaded", function() {
 
     onAuthStateChanged(auth, async (user) => {
 
+       const homebtn = document.getElementById('home');
+
+homebtn.addEventListener('click', async () => {
+  //getting current user
+  const user = auth.currentUser;
+  console.log("clicked!")
+
+  if (user) {
+    try {
+      
+      const userRef = ref(realtimeDb, 'users/' + user.uid)
+
+      get(userRef).then((snapshot) => {
+        const userData = snapshot.val();
+        const role = userData.role;
+        if (role === "Manager") {
+          window.location.href = 'manager-main-page.html'
+        } else if (role === "HR") {
+          window.location.href = 'admin-main-page.html'
+        } else {
+          window.location.href = 'main-page.html'
+        }
+      });
+    } catch (error) {
+      console.error("Error getting user role:", error)
+    }
+  } else {
+    window.location.href = 'index.html'
+  }
+})
+
+
+     
         //code for timesheets html
         if (user) {
             const userId = user.uid;
