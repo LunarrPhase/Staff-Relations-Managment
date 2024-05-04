00bc7a58dd623e2d09fb5cfa192bc5395422349f
NashTheHash <2554397@students.wits.ac.za>
2024-05-04 18:32:42 +0200

    Fixed sign in

--- src/sign-up.js
+++ src/sign-up.js
@@ -1,34 +1,10 @@
 // Import the functions you need from the SDKs you need
-//import { app } from "./initialise-firebase.js";
-import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { database, auth } from "./initialise-firebase.js";
+import { set, ref } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import { isValidAccessKey, SetRole, SetSignUpError } from "./functions.js";
 // TODO: Add SDKs for Firebase products that you w to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyCdhEnmKpeusKPs3W9sQ5AqpN5D62G5BlI",
-    authDomain: "staff-relations-management.firebaseapp.com",
-    databaseURL: "https://staff-relations-management-default-rtdb.firebaseio.com",
-    projectId: "staff-relations-management",
-    storageBucket: "staff-relations-management.appspot.com",
-    messagingSenderId: "5650617468",
-    appId: "1:5650617468:web:4892625924b0cf6b3ee5f9",
-    measurementId: "G-7J5915RDP9"
-};
-
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const database = getDatabase(app);
-const auth = getAuth();
 
 
 signUp.addEventListener('click', (e) =>{
@@ -40,6 +16,7 @@ signUp.addEventListener('click', (e) =>{
     let firstName = document.getElementById('firstName').value;
     let lastName = document.getElementById('lastName').value;
     let accessKey = document.getElementById('accessKey').value;
+    let role;
 
     if(document.getElementById('firstName').value.trim() === "" || document.getElementById('lastName').value.trim() === "" ){
         document.getElementById('error-message').textContent = "Enter a valid first and last name."
@@ -51,28 +28,19 @@ signUp.addEventListener('click', (e) =>{
         .then((userCredential) => {
 
             const user = userCredential.user;
-            let role = SetRole(accessKey);
+            role = SetRole(accessKey);
 
-      // Update user profile with role
-      /* user.updateProfile({
-        displayName: role,
-      });*/
+            set(ref(database, 'users/' + user.uid), {
+                email: email,
+                firstName: firstName,
+                lastName: lastName,
+                role: role,
+            });
 
-
-        set(ref(database, 'users/' + user.uid), {
-            email: email,
-            firstName: firstName,
-            lastName: lastName,
-            role: role,
-        });
-      
-      //console.log(firstName)
-      //console.log(role)
-
-        document.getElementById("info").textContent = "Your account was successfully created. Go back to the sign in page and sign in.";
-        window.location.href = 'index.html'
-    })
-    .catch((error) => {
+            document.getElementById("info").textContent = "Your account was successfully created. Go back to the sign in page and sign in.";
+            window.location.href = 'index.html'
+        })
+        .catch((error) => {
             console.log(error.code);
             const errorMessage = SetSignUpError(error, email, password);
             const errorMessageElement = document.getElementById('error-message');
