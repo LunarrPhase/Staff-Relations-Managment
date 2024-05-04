08380d596a6cae45e4844ca973dcba3945665709
chloe900 <chloetdube@icloud.com>
2024-05-04 14:41:23 +0200

    added "authenticating..." after user logs in to let them know there is something happening

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { FirebaseLogin } from "./functions.js";
 
 login.addEventListener('click', async (e) => {
     e.preventDefault(); // Prevent the default form submission
-
+    document.getElementById("authenticating").style.display = "block";
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
 
