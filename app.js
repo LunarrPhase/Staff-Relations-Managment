c84fd9e4bdf13feebfc22a35468b44e87e7c3e21
chloe900 <151669713+chloe900@users.noreply.github.com>
2024-05-11 23:21:29 +0200

    trying to solve realtime db issues

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-var express = require("express");
-var app = express();
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js"
+import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js"
 
-app.set("port", process.env.PORT || 3000);
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCdhEnmKpeusKPs3W9sQ5AqpN5D62G5BlI",
+    authDomain: "staff-relations-management.firebaseapp.com",
+    databaseURL: "https://staff-relations-management-default-rtdb.firebaseio.com",
+    projectId: "staff-relations-management",
+    storageBucket: "staff-relations-management.appspot.com",
+    messagingSenderId: "5650617468",
+    appId: "1:5650617468:web:4892625924b0cf6b3ee5f9",
+    measurementId: "G-7J5915RDP9"
+};
 
-app.use(express.static("src"));
-app.use(express.static("test"));
-app.use(express.static("src/img"));
+const app = initializeApp(firebaseConfig)
 
-app.listen(app.get("port"), function(){
-    console.log("Server started on port " + app.get("port"));
-});
+const database = getDatabase(app)
+
+document.addEventListener('signup', (e) => {
+    const { email, firstName, lastName, role } = e.detail;
+    set(ref(database, 'users/' + user.uid), {
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+      role: role,
+    });
+  });
+
+get(ref(database, 'users')).then((snapshot) => {
+    if (snapshot.exists()) {
+      const data = snapshot.val();
+      console.log(data);
+    } else {
+      console.log('No data available');
+    }
+  }).catch((error) => {
+    console.error('Error reading data:', error);
+  });
