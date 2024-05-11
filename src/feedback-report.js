80132c9fcefb0925da823cf253f602fcbf1eacad
Javaluvr <chiloanedineo@icloud.com>
2024-05-12 01:55:39 +0200

    home button on feedback report works

--- src/feedback-report.js
+++ src/feedback-report.js
@@ -97,3 +97,42 @@ generateScreenReport.addEventListener('click', async () => {
         messageCell.textContent = message;
     });
 });
+
+
+
+const homebtn = document.getElementById('home');
+
+//home button functionality
+homebtn.addEventListener('click', async () => {
+
+    //getting current user
+    const user = auth.currentUser;
+    console.log("clicked!")
+
+    if (user) {
+        try {
+            const userRef = ref(realtimeDb, 'users/' + user.uid);
+
+            get(userRef).then((snapshot) => {
+                const userData = snapshot.val();
+                const role = userData.role;
+
+                if (role === "Manager") {
+                    window.location.href = 'manager-main-page.html'
+                }
+                else if (role === "HR") {
+                    window.location.href = 'admin-main-page.html'
+                }
+                else {
+                    window.location.href = 'main-page.html'
+                }
+            });
+        }
+        catch (error) {
+            console.error("Error getting user role:", error)
+        }
+    }
+    else {
+        window.location.href = 'index.html'
+    }
+})
