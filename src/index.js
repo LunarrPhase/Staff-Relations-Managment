ed113e09b6abb83a7542bfcdd6ed71c86f0c5ffc
NashTheHash <2554397@students.wits.ac.za>
2024-05-18 21:26:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import { database, auth, firestore as db } from "./firebaseInit.js"
 import { FirebaseLogin } from "./firebase_functions.js";
 
 
-
 auth.signOut().then(() => {
     console.log('User signed out successfully');
     // Optionally, you can do additional actions here if needed
 }).catch((error) => {
     console.error('Error signing out: ', error);
 });
-
-
-
-
 
 
 //listens for the correct login credentials
