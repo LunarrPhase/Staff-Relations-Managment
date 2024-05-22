4cc0699aa53528ec62fad01a214f125d1bd899e7
NashTheHash <2554397@students.wits.ac.za>
2024-05-22 23:14:40 +0200

    commmit

--- __tests__/firebase_tests/all-notifs.test.js
+++ __tests__/firebase_tests/all-notifs.test.js
@@ -1,8 +1,18 @@
-import { SendHome, GetCurrentUserMealBookings, GetCurrentUserCarWashBookings } from "../../src/firebase_functions";
+import { SendHome, GetCurrentUserMealBookings, GetCurrentUserCarWashBookings, GetCurrentUserFeedbackNotifications } from "../../src/firebase_functions";
 import { mockFunctions } from "../../mocks";
 
 
 describe("Send Home functionality", () => {
+
+    let consoleSpy; 
+
+    beforeEach(() => {
+        consoleSpy = jest.spyOn(console, "error");
+    });
+
+    afterEach(() => {
+        console.error.mockRestore()
+    });
 
     it("Calls ChangeWindow when user is valid", async () => {
 
@@ -14,7 +24,6 @@ describe("Send Home functionality", () => {
 
     it("Throws an error when user role is not found", async () => {
 
-        const consoleSpy = jest.spyOn(console, 'error');
         const mockUser = { uid: null };
         await SendHome(mockUser);
         expect(consoleSpy).toHaveBeenCalledWith("Error getting user role:", "ID not found");
@@ -35,17 +44,25 @@ describe("Send Home functionality", () => {
 
 describe("Getting current meal bookings", () => {
 
+    let consoleSpy; 
+
+    beforeEach(() => {
+        consoleSpy = jest.spyOn(console, "error");
+    });
+
+    afterEach(() => {
+        console.error.mockRestore()
+    });
+
     it ("Throws error when user ID is invalid and calls Send Home", async () => {
 
-        const consoleSpy = jest.spyOn(console, "error");
         const mockUser = { uid: null };
         await GetCurrentUserMealBookings(mockUser);
-
         expect(consoleSpy).toHaveBeenCalledWith("User ID not available");
         expect(consoleSpy).toHaveBeenCalledWith("Error getting user role:", "ID not found");
     });
 
-    it ("Returns an array when user ID is valid", async () => {
+    it ("Returns an array of meal bookings when user ID is valid", async () => {
 
         const mockUser = { uid: "validID" };
         const mealBookings = await GetCurrentUserMealBookings(mockUser);
@@ -56,18 +73,44 @@ describe("Getting current meal bookings", () => {
 
 describe("Getting current carwash bookings", () => {
 
-    it ("Throws an error when user ID is invalid", async () => {
-
-        const consoleSpy = jest.spyOn(console, 'error');
-        const mockUser = { uid: null };
-        await GetCurrentUserCarWashBookings(mockUser);
-        expect(consoleSpy).toHaveBeenCalledWith("Error getting user role:", "ID not found");
-    })
-
-    it ("Returns an empty array when there are no meal bookings", async () => {
+    it ("Returns an array of carwash bookings", async () => {
 
         const mockUser = { uid: "validID" };
         const carWashBookings = await GetCurrentUserCarWashBookings(mockUser);
         expect(carWashBookings).toStrictEqual([ "fullOfBookings" ]);
+    });
+});
+
+
+describe("Getting current user feedback notifications", () =>{ 
+
+    let consoleSpy; 
+
+    beforeEach(() => {
+        consoleSpy = jest.spyOn(console, "error");
+    });
+
+    afterEach(() => {
+        console.error.mockRestore()
+    });
+
+    it("Throws an error if the input user email does not exist", async () => {
+
+        const userFeedback = await GetCurrentUserFeedbackNotifications(false);
+        expect(consoleSpy).toHaveBeenCalledWith("User email not available");
+        expect(userFeedback).toStrictEqual([]);
+    });
+
+    it("Returns an array of feedback notifications", async () => {
+
+        const userFeedback = await GetCurrentUserFeedbackNotifications("anemail@email.com");
+        expect(userFeedback).toStrictEqual([ "fullOfBookings" ]);
+    });
+
+    it("Throws an error when the email has the database return a querying error", async () => {
+
+        const userFeedback = await GetCurrentUserFeedbackNotifications("invalid_query@database.com");
+        expect(consoleSpy).toHaveBeenCalledWith("Error fetching feedback notifications:", "Querying error");
+        expect(userFeedback).toStrictEqual([]);
     })
 })
